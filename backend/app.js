3aac0be197caca5659eba1a0b523d6dbcb8db912
SergioGarciaTec <159815841+SergioGarciaTec@users.noreply.github.com>
2024-04-25 10:51:27 -0700

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,38 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+//Conexion a MySql
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ' ',
+    database: 'pwdata',
+    port: '3306'
+});
+
+//Nos conectamos a MySql
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('Conectado a la base de datos');
+    }
+})
 //Rutas de acceso
 app.get("/",function(req,res){
     res.send("Rutassssss de iniciosssss");
 })
+
+//Seleccionamos todos los clientes
+app.get('/api/clientes',(req,res)=>{
+    conexion.query('SELECT * FROM clientes',(error,filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+});
 
 //Encender servidor
 let puerto = 3000;
